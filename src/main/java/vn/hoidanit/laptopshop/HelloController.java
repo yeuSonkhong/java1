dd1b484b877beca7420ab2c375df2edc77c78a14
unknown <tranthanhsonforschool@gmail.com>
2025-08-23 22:25:54 +0700

    buoi 3

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World!";
+        return "Hello World thanh son!";
     }
 
     @GetMapping("/user")
--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,17 +2,13 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
 
-		ApplicationContext hoidanit = SpringApplication.run(LaptopshopApplication.class, args);
-		for (String s : hoidanit.getBeanDefinitionNames()) {
-			System.out.println(s);
-		}
-	}
+		SpringApplication.run(LaptopshopApplication.class, args);
 
+	}
 }
