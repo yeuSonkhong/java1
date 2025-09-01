e5998590f6562468d4fe2c78a6f94c5b02f52690
unknown <tranthanhsonforschool@gmail.com>
2025-09-01 09:26:09 +0700

    51

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -27,6 +27,14 @@ public class UserController {
         model.addAttribute("hoidanit", "from controller with love");
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "from controller with love");
+        return "admin/user/create";
+    }
 }
 
 // @RestController
--- src/main/webapp/WEB-INF/view/hello.jsp
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -11,7 +11,7 @@
             <!-- Latest compiled JavaScript -->
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-            <link href="/css/demo.css" rel="stylesheet">
+            <!--<link href="/css/demo.css" rel="stylesheet">-->
         </head>
 
         <body>
